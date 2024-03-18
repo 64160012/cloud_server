ef07ed6be969ad5366e6f0018a686bb33271ea80
neeracha-nrch <neeracha.3108@gmail.com>
2024-03-18 17:48:12 +0700

    3 commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 
 const app = express()
-const port = 3000
+const port = 80
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
